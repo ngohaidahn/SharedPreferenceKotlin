c483d27db2df7b9b8644ee5087eedb4a7edeb6ac
Ngo Hai <ngohai13061998@gmail.com>
2024-05-30 13:38:35 +0700

    kotlin

---
+++ app/src/main/java/com/example/sharepreferencesandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sharepreferencesandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+       
+
+    }
+}
